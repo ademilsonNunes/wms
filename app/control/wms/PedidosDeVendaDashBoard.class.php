2eba3ffb3b029ca0f2bed262c6d9e6bb1d95ccc8
= <=>
2021-04-24 16:28:10 -0300

    update pbi tools

--- app/control/wms/PedidosDeVendaDashBoard.class.php
+++ app/control/wms/PedidosDeVendaDashBoard.class.php
@@ -1,15 +1,20 @@
 <?php
+
+use Adianti\Control\TPage;
+use Adianti\Widget\Base\TElement;
+use Adianti\Widget\Container\TVBox;
+
 /**
- * EstoqueAtualDashboard
+ * PedidosDeVendaDashBoard
  *
  * @version    1.0
- * @package    EstoqueAtualDashboard
+ * @package    PedidosDeVendaDashBoard
  * @subpackage wms
  * @author     Ademilson NUnes
  * @copyright  Copyright (c) 2021 Sobel Suprema Insdustria de produtos de limpeza LTDA. (http://www.sobelsuprema.com.br)
  * @license    http://www.adianti.com.br/framework-license
  */
-class EstoqueAtualDashboard extends TPage
+class PedidosDeVendaDashBoard extends TPage
 { 
     /**
      * Page constructor
@@ -17,11 +22,11 @@ class EstoqueAtualDashboard extends TPage
     function __construct()
     {
         parent::__construct();
-      
+       
         $frame = new TElement('iframe');
         $frame->width  = '1259';
         $frame->height = '1020';
-        $frame->src = 'https://app.powerbi.com/view?r=eyJrIjoiZGY0MTU4OWEtMTIzNi00ODhhLWJjZTQtYjYyODAwMDhmYTJhIiwidCI6Ijc0MjgwOGM5LWQyMjEtNGI0OS05NTc0LTY2MjBlZTY2YmYwZSJ9';
+        $frame->src = 'https://app.powerbi.com/view?r=eyJrIjoiZGEzNWU0MTEtOTY4ZS00MDkzLTkxZmYtMGZiZmZkOWY0OGM0IiwidCI6Ijc0MjgwOGM5LWQyMjEtNGI0OS05NTc0LTY2MjBlZTY2YmYwZSJ9&pageName=ReportSection';
         $frame->frameborder = '0';
         $frame->allowFullScreen = 'true';
 
@@ -30,8 +35,5 @@ class EstoqueAtualDashboard extends TPage
         $vbox->style = 'width: 100%';
         $vbox->add($frame);
         parent::add($vbox);
-
-    }
-
-    
+    }       
 }
